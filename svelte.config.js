096739a777ad7f82f125772c76ffa131bc6ced2c
tonymushah <tonyramanantsalama@gmail.com>
2024-06-01 09:17:00 +0300

    Added App dir

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		files: {
+			hooks: {
+				client: 'app/hooks.client',
+				server: 'app/hooks.server',
+				universal: 'app/hooks'
+			},
+			lib: 'app/lib',
+			params: 'app/params',
+			routes: 'app/routes',
+			serviceWorker: 'app/service-worker',
+			appTemplate: 'app/app.html',
+			errorTemplate: 'app/error.html'
+		}
 	}
 };
 
