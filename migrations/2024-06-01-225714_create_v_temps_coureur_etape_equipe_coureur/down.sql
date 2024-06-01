a3921c05c4e61036d489f84734787727e29d4c4d
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 02:02:45 +0300

    Added `2024-06-01-225714_create_v_temps_coureur_etape_equipe_coureur` migration

---
+++ migrations/2024-06-01-225714_create_v_temps_coureur_etape_equipe_coureur/down.sql
@@ -0,0 +1,2 @@
+-- This file should undo anything in `up.sql`
+drop VIEW v_temps_coureur_etape_equipe_coureur;
--- src/view.rs
+++ src/view.rs
@@ -9,3 +9,17 @@ diesel::table! {
         equipe_coureur -> Uuid
     }
 }
+
+diesel::table! {
+    v_temps_coureur_etape_equipe_coureur(etape, equipe_coureur, coureur, equipe) {
+        etape -> Int4,
+        longueur -> Numeric,
+        nom -> Text,
+        nb_coureur_par_equipe -> Int4,
+        temps -> Int4,
+        points -> Int4,
+        equipe_coureur -> Uuid,
+        coureur -> Uuid,
+        equipe -> Uuid
+    }
+}
