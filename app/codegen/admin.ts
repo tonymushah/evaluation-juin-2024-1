2f9ecbd39d8a32278fd43a86c41815915b1cd295
tonymushah <tonyramanantsalama@gmail.com>
2024-06-01 11:16:29 +0300

    Admin codegen refactor

--- app/codegen/admin.ts
+++ app/codegen/admin.ts
@@ -1,16 +1,20 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
+const BASE = 'admin';
+
+const GEN = `app/lib/${BASE}/gql/`;
+
 const config: CodegenConfig = {
-	schema: './app/schemas/admin.graphqls',
+	schema: `./app/schemas/${BASE}.graphqls`,
 	documents: [
-		'app/lib/admin/**/*.svelte',
-		'app/lib/admin/**/*.ts',
-		'app/routes/admin/**/*.svelte',
-		'app/routes/admin/**/*.ts'
+		`app/lib/${BASE}/**/*.svelte`,
+		`app/lib/${BASE}/**/*.ts`,
+		`app/routes/${BASE}/**/*.svelte`,
+		`app/routes/${BASE}/**/*.ts`
 	],
 	ignoreNoDocuments: true, // for better experience with the watcher
 	generates: {
-		'app/lib/admin/gql/': {
+		GEN: {
 			preset: 'client',
 			config: {
 				useTypeImports: true
