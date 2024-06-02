072aae5146e731a03058327d9bac3ae0ff185c92
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 14:56:13 +0300

    Added `affecter joueur to etape` componnent

---
+++ app/lib/equipe/componnents/etape/page/CoueurSelector.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let nom: string;
+	export let isSelected: boolean = false;
+	const dispatch = createEventDispatcher<{
+		click: MouseEvent & {
+			currentTarget: EventTarget & HTMLDivElement;
+		};
+		keydown: KeyboardEvent & {
+			currentTarget: EventTarget & HTMLDivElement;
+		};
+	}>();
+</script>
+
+<div
+	class="flex rounded bg-slate-300 transition-colors hover:bg-slate-400 p-6 justify-center items-center"
+	class:selected={isSelected}
+	role="button"
+	on:click={(e) => {
+		dispatch('click', e);
+	}}
+	tabindex="0"
+	on:keydown={(e) => {
+		dispatch('keydown', e);
+	}}
+>
+	{nom}
+</div>
+
+<style lang="postcss">
+	.selected {
+		background-color: theme('colors.orange.300');
+	}
+	.selected:hover {
+		background-color: theme('colors.orange.400');
+	}
+</style>
---
+++ app/routes/equipe/etape/[id]/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import CoueurSelector from '$lib/equipe/componnents/etape/page/CoueurSelector.svelte';
+	import type { ComponentProps } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const testData = writable<ComponentProps<CoueurSelector>[]>([
+		{
+			nom: 'Tony',
+			isSelected: false
+		},
+		{
+			nom: 'Akari',
+			isSelected: false
+		},
+		{
+			nom: 'Tomfy',
+			isSelected: false
+		}
+	]);
+</script>
+
+<div class="grid grid-cols-2">
+	<h2 class="text-xl font-semibold underline">%Nom Etape%</h2>
+	<p class="text-lg">Points: %nombre_points% pts</p>
+</div>
+
+<div class="flex flex-wrap gap-2 mt-3">
+	{#each $testData as coueur}
+		<CoueurSelector
+			{...coueur}
+			on:click={() => {
+				testData.update((c) => {
+					const selected = c.find((c) => c.nom == coueur.nom);
+					if (selected) {
+						selected.isSelected = !selected.isSelected;
+					}
+					return c;
+				});
+			}}
+		/>
+	{/each}
+</div>
