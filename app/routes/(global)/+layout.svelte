b2c6a37815a79501fcfff56e5ec0faf602083d1e
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 12:10:42 +0300

    Added `brand` $lib var

---
+++ app/routes/(global)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { brand } from '$lib';
+	import IndexNavbar from '$lib/global/componnents/IndexNavbar.svelte';
+</script>
+
+<IndexNavbar />
+
+<svelte:head>
+	<title>{brand}</title>
+</svelte:head>
+
+<div class="content ml-5 mr-5 mt-5">
+	<slot />
+</div>
