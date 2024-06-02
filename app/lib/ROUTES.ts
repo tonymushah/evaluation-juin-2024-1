3396cae38d2f12edcd50043389901db870ea0c7a
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 10:42:50 +0300

    Added `fonts`

--- app/lib/ROUTES.ts
+++ app/lib/ROUTES.ts
@@ -9,7 +9,8 @@
  * PAGES
  */
 const PAGES = {
-  "/": `/`
+  "/": `/`,
+  "/admin/login": `/admin/login`
 }
 
 /**
@@ -133,7 +134,7 @@ export function route<T extends keyof AllTypes>(key: T, ...params: any[]): strin
 * ```
 */
 export type KIT_ROUTES = {
-  PAGES: { '/': never }
+  PAGES: { '/': never, '/admin/login': never }
   SERVERS: Record<string, never>
   ACTIONS: Record<string, never>
   LINKS: Record<string, never>
--- app/routes/+layout.svelte
+++ app/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import '@fontsource-variable/inter';
+	import '@fontsource/fira-mono';
+	import '@fontsource/poppins/100.css';
+	import '@fontsource/poppins/200.css';
+	import '@fontsource/poppins/300.css';
+	import '@fontsource/poppins/400.css';
+	import '@fontsource/poppins/500.css';
+	import '@fontsource/poppins/600.css';
+	import '@fontsource/poppins/700.css';
+	import '@fontsource/poppins/800.css';
+	import '@fontsource/poppins/900.css';
 </script>
 
 <slot />
