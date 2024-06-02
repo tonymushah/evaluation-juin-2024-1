c7cefe395fa4d30b556ef939630575afd747bcc2
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 12:11:17 +0300

    Added `classement globale` on the front-page

---
+++ app/lib/global/componnents/IndexNavbar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button, NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+
+	import { page } from '$app/stores';
+	import { route } from '$lib/ROUTES';
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar rounded class="bg-slate-300 dark:bg-slate-800">
+	<NavBrand href="/">
+		<img class="h-12" src="/logo2.png" alt="Ultimate Team Race" />
+	</NavBrand>
+	<div class="flex md:order-2">
+		<Button size="sm">Se connecter</Button>
+		<NavHamburger />
+	</div>
+	<h1 class="font-brand text-orange-600 font-bold text-4xl">Ultimate Team Race</h1>
+</Navbar>
---
+++ app/routes/(global)/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { TabItem, Table, Tabs } from 'flowbite-svelte';
+	import Generale, {
+		type ClassementItem as CGeneItem
+	} from '$lib/global/componnents/classement/general/TableClassmmentGenerale.svelte';
+	import Equipes, {
+		type ClassementItem as CEqupeItem
+	} from '$lib/global/componnents/classement/equipe/TableClassmmentEquipe.svelte';
+	import { readable } from 'svelte/store';
+	import { brand } from '$lib';
+
+	const coueur = readable<CGeneItem[]>([
+		{
+			coureur: 'Tony',
+			points: 100
+		}
+	]);
+	const equipes = readable<CEqupeItem[]>([]);
+</script>
+
+<h2 class="text-2xl font-bold underline">Classements global</h2>
+
+<svelte:head>
+	<title>Classement global | {brand}</title>
+</svelte:head>
+
+<Tabs class="mt-2">
+	<TabItem open title="Par Coureur">
+		<Generale classement={coueur} />
+	</TabItem>
+	<TabItem title="Par Equipe">
+		<Equipes />
+	</TabItem>
+</Tabs>
