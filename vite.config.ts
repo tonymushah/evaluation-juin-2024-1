64b05cd6630a189ff49fc63a581a17fe929644ba
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 10:18:06 +0300

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { kitRoutes } from 'vite-plugin-kit-routes';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		kitRoutes({
+			routes_path: './app/routes',
+			generated_file_path: 'app/lib/ROUTES.ts'
+		})
+	],
+	server: {
+		fs: {
+			deny: ['src'],
+			allow: ['app/app.css']
+		}
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['app/**/*.{test,spec}.{js,ts}']
+	},
+	optimizeDeps: {
+		exclude: ['@urql/svelte']
 	}
 });
