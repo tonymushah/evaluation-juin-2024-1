94ffce57f5ba1da8b40d99046b93cc9c5db01aa7
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 14:11:53 +0300

    Added EtapeCard

---
+++ app/lib/equipe/componnents/etape/EtapeCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { route } from '$lib/ROUTES';
+	import { Badge, Card } from 'flowbite-svelte';
+	import type { ComponentProps } from 'svelte';
+	import JoueurEtapeCard from './JoueurEtapeCard.svelte';
+	export let id: string;
+	export let etat: string;
+	export let nom: string;
+	export let points: number = 0;
+	export let joueurs: ComponentProps<JoueurEtapeCard>[] = [];
+</script>
+
+<div class="flex bg-slate-300 rounded flex-col">
+	<div class="flex flex-row justify-center items-center gap-2">
+		<Badge>{etat}</Badge>
+		<a href={route('/equipe/etape/[id]', { id })} class="hover:text-orange-400 transition-colors">
+			{nom}
+		</a>
+		<p>
+			Points: {points}
+		</p>
+	</div>
+	<div class="flex flex-wrap gap-2">
+		{#each joueurs as joueur}
+			<JoueurEtapeCard {...joueur} />
+		{/each}
+	</div>
+</div>
---
+++ app/lib/equipe/componnents/etape/JoueurEtapeCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let nom: string;
+	export let points: number = 0;
+</script>
+
+<div class="flex bg-cyan-200 dark:bg-cyan-800 rounded p-5">
+	<h4 class=" font-mono text-xl">{nom}</h4>
+	<p>Points: {points}</p>
+</div>
