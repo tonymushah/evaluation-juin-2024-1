dbb1c42e9e9569fdefc263d8e6c07bb33dc8f69d
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 03:56:20 +0300

    Added `global.ts` codegen

---
+++ app/codegen/global.ts
@@ -0,0 +1,25 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const BASE = 'global';
+
+const config: CodegenConfig = {
+	schema: `./app/schemas/${BASE}.graphqls`,
+	documents: [
+		`app/lib/${BASE}/**/*.svelte`,
+		`app/lib/${BASE}/**/*.ts`,
+		`app/routes/${BASE}/**/*.svelte`,
+		`app/routes/${BASE}/**/*.ts`
+	],
+	ignoreNoDocuments: true, // for better experience with the watcher
+	generates: {
+		[`app/lib/${BASE}/gql/`]: {
+			preset: 'client',
+			config: {
+				useTypeImports: true
+			},
+			plugins: []
+		}
+	}
+};
+
+export default config;
