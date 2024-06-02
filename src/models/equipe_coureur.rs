17cd94cd557b59a67acfe73559d4427ed3be1552
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 03:02:01 +0300

    Added `VEquipeCoureur`

--- src/models/equipe_coureur.rs
+++ src/models/equipe_coureur.rs
@@ -1,8 +1,9 @@
 use async_graphql::{InputObject, SimpleObject};
 use diesel::prelude::*;
+use time::Date;
 use uuid::Uuid;
 
-use crate::schema::equipe_coureur;
+use crate::{schema::equipe_coureur, view::v_equipe_coureur};
 
 #[derive(
     Debug,
@@ -28,4 +29,20 @@ pub struct EquipeCoureur {
     pub id_equipe_coureur: Uuid,
     pub coureur: i32,
     pub equipe: Uuid,
+}
+
+#[derive(
+    Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Identifiable, Selectable, Queryable, SimpleObject,
+)]
+#[diesel(table_name = v_equipe_coureur)]
+#[diesel(primary_key(coureur, equipe))]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct VEquipeCoureur {
+    pub coureur: i32,
+    pub equipe: Uuid,
+    pub nom_coureur: String,
+    pub genre: i32,
+    pub dtn: Date,
+    pub pseudo_equipe: String,
+    pub nom_equipe: String,
 }
