52ea04cc63f5532f612ac94ca20159a50861235a
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 10:59:04 +0300

    Added `admin/login` skeleton page

---
+++ app/routes/admin/login/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Label, Input, Button } from 'flowbite-svelte';
+</script>
+
+<div class="flex w-screen h-screen items-center justify-center">
+	<div class="w-2/3 h-2/3 rounded-md bg-slate-300 dark:bg-slate-700 p-4 grid grid-cols-2">
+		<div class="flex items-center justify-center w-full h-full">
+			<img src="/logo1.png" alt="logo" />
+		</div>
+		<div class="flex w-full h-full items-center justify-center flex-col gap-5">
+			<h2 class="dark:text-white text-2xl font-bold">Se Connecter antant qu'admin</h2>
+			<form method="post" class=" w-11/12 flex flex-col gap-5">
+				<div>
+					<Label class="mb-2" for="nom_user">Nom d'utilisateur</Label>
+					<Input name="nom" type="text" id="nom_user" placeholder="Admin" required />
+				</div>
+				<div>
+					<Label class="mb-2" for="password">Mot de passe</Label>
+					<Input name="password" type="text" id="password" placeholder="Password" required />
+				</div>
+				<Button type="submit">Se Connecter</Button>
+			</form>
+		</div>
+	</div>
+</div>
