38ee29b67cf11fd7fed353652de1a8da792ffb94
tonymushah <tonyramanantsalama@gmail.com>
2024-06-02 11:21:20 +0300

    Added `TableClassementGenerale`

---
+++ app/lib/global/componnents/classement/general/TClassHead.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { TableHead, TableHeadCell } from 'flowbite-svelte';
+</script>
+
+<TableHead>
+	<TableHeadCell>Coureur</TableHeadCell>
+	<TableHeadCell>Point</TableHeadCell>
+</TableHead>
---
+++ app/lib/global/componnents/classement/general/TClassRow.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { TableBodyCell, TableBodyRow } from 'flowbite-svelte';
+	export let coureur: string;
+	export let points: number;
+	$: isPoints = points > 1;
+</script>
+
+<TableBodyRow>
+	<TableBodyCell>{coureur}</TableBodyCell>
+	<TableBodyCell>
+		{points}
+		<span>
+			{#if isPoints}
+				points
+			{:else}
+				point
+			{/if}
+		</span>
+	</TableBodyCell>
+</TableBodyRow>
