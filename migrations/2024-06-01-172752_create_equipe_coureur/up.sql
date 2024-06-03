a6b521b4996b509798eb6b8268c2232f60a488af
tonymushah <tonyramanantsalama@gmail.com>
2024-06-03 09:43:40 +0300

    equipe coureur table updates

--- migrations/2024-06-01-172752_create_equipe_coureur/up.sql
+++ migrations/2024-06-01-172752_create_equipe_coureur/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 create table equipe_coureur(
     id_equipe_coureur UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    coureur INT NOT NULL REFERENCES coureur(numero_dosard),
+    coureur INT NOT NULL UNIQUE REFERENCES coureur(numero_dosard),
     equipe UUID NOT NULL REFERENCES equipe(id_equipe)
 );
--- migrations/j2_up.sql
+++ migrations/j2_up.sql
@@ -37,7 +37,7 @@ create table coureur_categorie(
 
 create table equipe_coureur(
     id_equipe_coureur UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    coureur INT NOT NULL REFERENCES coureur(numero_dosard),
+    coureur INT NOT NULL UNIQUE REFERENCES coureur(numero_dosard),
     equipe UUID NOT NULL REFERENCES equipe(id_equipe)
 );
 
