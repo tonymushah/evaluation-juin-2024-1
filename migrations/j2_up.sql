e9cbab07a745a1ed4493a16640730553dcb61a8a
tonymushah <tonyramanantsalama@gmail.com>
2024-06-03 19:33:52 +0300

    j2_up.sql update

--- migrations/j2_up.sql
+++ migrations/j2_up.sql
@@ -162,3 +162,25 @@ FROM
 	COUREUR_CATEGORIE AS CC
 	JOin coureur as crr on cc.coureur = crr.numero_dosard
 	JOIN categorie as cat on cc.categorie = cat.id_categorie;
+
+Create view V_temps_coureur_etape_equipe_coureur_categorie as
+SELECT
+	TCEEC.ETAPE,
+	TCEEC.LONGUEUR,
+	TCEEC.NOM AS ETAPE_NOM,
+	TCEEC.NB_COUREUR_PAR_EQUIPE AS NB_PAR_EQUIPE,
+	TCEEC.TEMPS,
+	TCEEC.POINTS,
+	TCEEC.EQUIPE_COUREUR,
+	TCEEC.COUREUR,
+	TCEEC.EQUIPE,
+	crr_cat.nom as nom_coureur,
+	crr_cat.genre as genre,
+	crr_cat.categorie as categorie,
+	crr_cat.id_cc,
+	crr_cat.dtn,
+	crr_cat.designation
+FROM
+	V_coureur_categorie as crr_cat
+	JOIN V_TEMPS_COUREUR_ETAPE_EQUIPE_COUREUR AS TCEEC
+	On TCEEC.coureur = crr_cat.coureur;
