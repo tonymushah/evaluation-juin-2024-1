ce467e66359a50b8ffed7cd7a20b69d98f960319
tonymushah <tonyramanantsalama@gmail.com>
2024-06-03 08:29:04 +0300

    j1_up_revert

--- migrations/j1_up.sql
+++ migrations/j1_up.sql
@@ -46,8 +46,7 @@ create table etape(
     rang SERIAL PRIMARY KEY,
     longueur DECIMAL NOT NULL,
     nom TEXT NOT NULL,
-    nb_coureur_par_equipe INT NOT NULL,
-	depart TIMESTAMP not NULL
+    nb_coureur_par_equipe INT NOT NULL
 );
 
 create table temps_coureur(
