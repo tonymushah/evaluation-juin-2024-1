d0f5845a5f0267ae3605f91a7481329ce5d62edd
tonymushah <tonyramanantsalama@gmail.com>
2024-06-05 13:27:11 +0300

    Added `SystemCategories::seed` in reset_db

--- src/reset.rs
+++ src/reset.rs
@@ -2,10 +2,11 @@ use std::env;
 
 use diesel::{dsl::sql, sql_types::Untyped, RunQueryDsl};
 
-use crate::DbPoolConnection;
+use crate::{modules::generate_categories::SystemCategories, DbPoolConnection};
 
 pub fn reset_db(con: &mut DbPoolConnection) -> crate::Result<()> {
     let user = env::var("DBUSER")?;
     sql::<Untyped>(format!("SELECT truncate_tables('{user}');").as_str()).execute(con)?;
+    SystemCategories::seed(con)?;
     Ok(())
 }
