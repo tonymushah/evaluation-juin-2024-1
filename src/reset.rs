64a6311ace09fd89b4f55d971a0713c0324a1e89
tonymushah <tonyramanantsalama@gmail.com>
2024-06-05 13:30:36 +0300

    used transaction for `reset_db`

--- src/reset.rs
+++ src/reset.rs
@@ -1,12 +1,12 @@
 use std::env;
 
-use diesel::{dsl::sql, sql_types::Untyped, RunQueryDsl};
+use diesel::{dsl::sql, sql_types::Untyped, Connection, RunQueryDsl};
 
 use crate::{modules::generate_categories::SystemCategories, DbPoolConnection};
 
 pub fn reset_db(con: &mut DbPoolConnection) -> crate::Result<()> {
     let user = env::var("DBUSER")?;
     sql::<Untyped>(format!("SELECT truncate_tables('{user}');").as_str()).execute(con)?;
-    SystemCategories::seed(con)?;
+    con.transaction(|con| SystemCategories::seed(con))?;
     Ok(())
 }
