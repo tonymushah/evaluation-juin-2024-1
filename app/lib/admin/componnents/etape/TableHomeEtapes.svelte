501ef1198e00d7bdf96577c24a996045ecd101e2
tonymushah <tonyramanantsalama@gmail.com>
2024-06-05 11:33:03 +0300

    some penalite fixes

--- app/lib/admin/componnents/etape/TableHomeEtapes.svelte
+++ app/lib/admin/componnents/etape/TableHomeEtapes.svelte
@@ -16,28 +16,34 @@
 
 <Button disabled={$isLoading} on:click={reset}>Reset</Button>
 
-<Table color="blue">
-	<TableHead>
-		<TableHeadCell>Rang</TableHeadCell>
-		<TableHeadCell>Nom etape</TableHeadCell>
-		<TableHeadCell>Nb Coureurs</TableHeadCell>
-		<TableHeadCell>Kilometrage</TableHeadCell>
-		<TableHeadCell>Date Debut</TableHeadCell>
-		<TableHeadCell>Date Fin</TableHeadCell>
-	</TableHead>
-	<TableBody tableBodyClass="divide-y">
-		{#each $data as class_}
-			<TableEtapeRow
-				rang={class_.rang}
-				nom={class_.nom}
-				kilometrage={class_.longueur}
-				dateDebut={class_.depart}
-				dateFin={class_.finished}
-				nbCoureur={class_.nbCoureur}
-			/>
-		{/each}
-	</TableBody>
-</Table>
+{#if $data.length > 0}
+	<Table color="blue">
+		<TableHead>
+			<TableHeadCell>Rang</TableHeadCell>
+			<TableHeadCell>Nom etape</TableHeadCell>
+			<TableHeadCell>Nb Coureurs</TableHeadCell>
+			<TableHeadCell>Kilometrage</TableHeadCell>
+			<TableHeadCell>Date Debut</TableHeadCell>
+			<TableHeadCell>Date Fin</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			{#each $data as class_}
+				<TableEtapeRow
+					rang={class_.rang}
+					nom={class_.nom}
+					kilometrage={class_.longueur}
+					dateDebut={class_.depart}
+					dateFin={class_.finished}
+					nbCoureur={class_.nbCoureur}
+				/>
+			{/each}
+		</TableBody>
+	</Table>
+{:else}
+	<div class="flex w-full justify-center items-center">
+		<h2 class="text-xl">Rien a voir??</h2>
+	</div>
+{/if}
 
 {#if $hasNext && !$isLoading}
 	<div class="flex w-full items-center justify-center">
