7b7b03aeb5fb3d99c4a3c8934183d64bec6140d6
tonymushah <tonyramanantsalama@gmail.com>
2024-06-09 19:49:08 +0300

    Some page serverload finished

--- app/routes/equipe/logout/+page.server.ts
+++ app/routes/equipe/logout/+page.server.ts
@@ -1,11 +1,9 @@
 import { CLIENT_TOKEN_KEY } from '$lib/equipe/utils/tokenKey';
 import { redirect } from '@sveltejs/kit';
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { route } from '$lib/ROUTES';
 
-export const actions = {
-	default: async ({ cookies }) => {
-		cookies.delete(CLIENT_TOKEN_KEY, { path: '/equipe' });
-		return redirect(300, route('/equipe/login'));
-	}
-} satisfies Actions;
+export const load: PageServerLoad = async ({ cookies }) => {
+	cookies.delete(CLIENT_TOKEN_KEY, { path: '/equipe' });
+	return redirect(300, route('/equipe/login'));
+};
