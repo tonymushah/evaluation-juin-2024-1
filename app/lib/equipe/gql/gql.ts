2de387236f64fa2b731f26c23dd496dd0283ec11
tonymushah <tonyramanantsalama@gmail.com>
2024-06-11 05:50:39 +0300

    Added selected context

--- app/lib/equipe/gql/gql.ts
+++ app/lib/equipe/gql/gql.ts
@@ -17,6 +17,8 @@ const documents = {
     "\n\tquery etapeCoureurs($etape: Int!) {\n\t\tetape {\n\t\t\tjoueurs(etape: $etape) {\n\t\t\t\tcoureur\n\t\t\t\tnomCoureur\n\t\t\t\tpoints\n\t\t\t}\n\t\t}\n\t}\n": types.EtapeCoureursDocument,
     "\n\tquery listEtapes($page: OffsetLimit!) {\n\t\tetape {\n\t\t\tlist(pagination: $page) {\n\t\t\t\tdata {\n\t\t\t\t\trang\n\t\t\t\t\tlongueur\n\t\t\t\t\tnom\n\t\t\t\t\tdepart\n\t\t\t\t\tfinished\n\t\t\t\t}\n\t\t\t\tlimit\n\t\t\t\toffset\n\t\t\t\ttotal\n\t\t\t}\n\t\t}\n\t}\n": types.ListEtapesDocument,
     "\n\tquery currentEquipe {\n\t\tcurrent {\n\t\t\tpseudo\n\t\t\tnom\n\t\t\tidEquipe\n\t\t}\n\t}\n": types.CurrentEquipeDocument,
+    "\n\t\tmutation addCoureurToEtape($etape: Int!, $joueur: Int!) {\n\t\t\tajouterJoueurToEtape(etape: $etape, joueur: $joueur) {\n\t\t\t\tidTempsCoureur\n\t\t\t\ttemps\n\t\t\t\tpoints\n\t\t\t\tequipeCoureur\n\t\t\t\tetape\n\t\t\t}\n\t\t}\n\t": types.AddCoureurToEtapeDocument,
+    "\n\t\tmutation removeCoureurToEtape($etape: Int!, $joueur: Int!) {\n\t\t\tremoveJoueurToEtape(etape: $etape, joueur: $joueur) {\n\t\t\t\tidTempsCoureur\n\t\t\t\ttemps\n\t\t\t\tpoints\n\t\t\t\tequipeCoureur\n\t\t\t\tetape\n\t\t\t}\n\t\t}\n\t": types.RemoveCoureurToEtapeDocument,
     "\n\tmutation equipeLogin($user: String!, $password: String!) {\n\t\tlogin(username: $user, password: $password)\n\t}\n": types.EquipeLoginDocument,
 };
 
@@ -50,6 +52,14 @@ export function graphql(source: "\n\tquery listEtapes($page: OffsetLimit!) {\n\t
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n\tquery currentEquipe {\n\t\tcurrent {\n\t\t\tpseudo\n\t\t\tnom\n\t\t\tidEquipe\n\t\t}\n\t}\n"): (typeof documents)["\n\tquery currentEquipe {\n\t\tcurrent {\n\t\t\tpseudo\n\t\t\tnom\n\t\t\tidEquipe\n\t\t}\n\t}\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n\t\tmutation addCoureurToEtape($etape: Int!, $joueur: Int!) {\n\t\t\tajouterJoueurToEtape(etape: $etape, joueur: $joueur) {\n\t\t\t\tidTempsCoureur\n\t\t\t\ttemps\n\t\t\t\tpoints\n\t\t\t\tequipeCoureur\n\t\t\t\tetape\n\t\t\t}\n\t\t}\n\t"): (typeof documents)["\n\t\tmutation addCoureurToEtape($etape: Int!, $joueur: Int!) {\n\t\t\tajouterJoueurToEtape(etape: $etape, joueur: $joueur) {\n\t\t\t\tidTempsCoureur\n\t\t\t\ttemps\n\t\t\t\tpoints\n\t\t\t\tequipeCoureur\n\t\t\t\tetape\n\t\t\t}\n\t\t}\n\t"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n\t\tmutation removeCoureurToEtape($etape: Int!, $joueur: Int!) {\n\t\t\tremoveJoueurToEtape(etape: $etape, joueur: $joueur) {\n\t\t\t\tidTempsCoureur\n\t\t\t\ttemps\n\t\t\t\tpoints\n\t\t\t\tequipeCoureur\n\t\t\t\tetape\n\t\t\t}\n\t\t}\n\t"): (typeof documents)["\n\t\tmutation removeCoureurToEtape($etape: Int!, $joueur: Int!) {\n\t\t\tremoveJoueurToEtape(etape: $etape, joueur: $joueur) {\n\t\t\t\tidTempsCoureur\n\t\t\t\ttemps\n\t\t\t\tpoints\n\t\t\t\tequipeCoureur\n\t\t\t\tetape\n\t\t\t}\n\t\t}\n\t"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- app/routes/equipe/(connected)/etape/[id]/+layout.svelte
+++ app/routes/equipe/(connected)/etape/[id]/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" context="module">
-	import { writable, type Writable } from 'svelte/store';
+	import { derived, readonly, writable, type Readable, type Writable } from 'svelte/store';
 	import { getContext } from 'svelte';
+	import { query as joueursQuery } from '$lib/equipe/componnents/etape/joueurs.query';
 
 	const pointsContextKey = 'ETAPE-SUB-POINT-KEY';
 	export function getPointsContextKey(): Writable<number> {
@@ -11,13 +12,140 @@
 			throw new Error(`The ${pointsContextKey} should be defined`);
 		}
 	}
+	type SelectedJoueurMap = Map<number, boolean>;
+	type SelectedJoueurStoreExtra = {
+		select(coureur: number): Promise<void> | void;
+		unselect(coureur: number): Promise<void> | void;
+		listenToCoeur(coureur: number): Readable<boolean>;
+		start(): void | Promise<void>;
+	};
+	type SelectedJoueurStore = Readable<SelectedJoueurMap> & SelectedJoueurStoreExtra;
+	type SelectedJoueurStoreWritable = Writable<SelectedJoueurMap> & SelectedJoueurStoreExtra;
+
+	const selectedJoueursContextKey = 'SELECT-JOUEUR-KEY';
+	const addCoureurToEtapeMutation = graphql(`
+		mutation addCoureurToEtape($etape: Int!, $joueur: Int!) {
+			ajouterJoueurToEtape(etape: $etape, joueur: $joueur) {
+				idTempsCoureur
+				temps
+				points
+				equipeCoureur
+				etape
+			}
+		}
+	`);
+	const removeCoureurToEtapeMutation = graphql(`
+		mutation removeCoureurToEtape($etape: Int!, $joueur: Int!) {
+			removeJoueurToEtape(etape: $etape, joueur: $joueur) {
+				idTempsCoureur
+				temps
+				points
+				equipeCoureur
+				etape
+			}
+		}
+	`);
+	function initSelectedJoueurStore(client: Client, etape: number): SelectedJoueurStore {
+		let isLoading = false;
+		const write = writable<SelectedJoueurMap>(new Map(), (set, update) => {
+			isLoading = true;
+			const sub = client
+				.query(joueursQuery, {
+					etape
+				})
+				.subscribe((res) => {
+					const joueurs = res.data?.etape.joueurs;
+					if (joueurs) {
+						update((map) => {
+							joueurs.forEach((j) => {
+								map.set(j.coureur, true);
+							});
+							return map;
+						});
+						isLoading = false;
+					} else if (res.error) {
+						isLoading = false;
+					}
+				});
+			return () => {
+				sub.unsubscribe();
+			};
+		});
+		async function start() {
+			if (!isLoading) {
+				isLoading = true;
+				await client
+					.query(joueursQuery, {
+						etape
+					})
+					.then((res) => {
+						const joueurs = res.data?.etape.joueurs;
+						if (joueurs) {
+							write.update((map) => {
+								joueurs.forEach((j) => {
+									map.set(j.coureur, true);
+								});
+								return map;
+							});
+						}
+					});
+				isLoading = false;
+			}
+		}
+		const read = readonly(write);
+		return setContext(selectedJoueursContextKey, {
+			async select(coureur) {
+				if (!isLoading) {
+					isLoading = true;
+					await client
+						.mutation(addCoureurToEtapeMutation, {
+							joueur: coureur,
+							etape
+						})
+						.toPromise()
+						.finally(() => (isLoading = false));
+					await this.start();
+				}
+			},
+			async unselect(coureur) {
+				if (!isLoading) {
+					isLoading = true;
+					await client
+						.mutation(removeCoureurToEtapeMutation, {
+							joueur: coureur,
+							etape
+						})
+						.toPromise()
+						.finally(() => (isLoading = false));
+					await this.start();
+				}
+			},
+			listenToCoeur(coureur) {
+				return derived(write, (v) => v.get(coureur) ?? false);
+			},
+			start,
+			...read
+		});
+	}
+	export function getSelectedJoueurStore(): SelectedJoueurStore {
+		const context = getContext<SelectedJoueurStore | undefined>(selectedJoueursContextKey);
+		if (context) {
+			return context;
+		} else {
+			throw new Error(`The ${selectedJoueursContextKey} should be defined`);
+		}
+	}
 </script>
 
 <script lang="ts">
 	import type { LayoutServerData } from './$types';
 	import { setContext } from 'svelte';
+	import { getContextClient, type Client } from '@urql/svelte';
+	import { graphql } from '$lib/equipe/gql';
 	export let data: LayoutServerData;
+	const client = getContextClient();
 	const points = setContext(pointsContextKey, writable(data.points));
+	const selecteds = initSelectedJoueurStore(client, data.rang);
 </script>
 
 <div class="grid grid-cols-2">
